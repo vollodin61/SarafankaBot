e1ea4368fc38c08bdab480671347e742b94a16da
ilia_volodin <vollodin61@gmail.com>
2023-12-05 17:41:12 +0300

    clear main it commit like init commit

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,5 @@
 import asyncio
 import logging
-import re
-from datetime import datetime
 
 from aiogram import Bot, Dispatcher, html, F
 from aiogram.enums import ParseMode
@@ -15,20 +13,15 @@ from aiogram.utils.formatting import as_list, as_marked_section, Bold, as_key_va
 from aiogram.utils.markdown import hide_link
 from aiogram.utils.media_group import MediaGroupBuilder
 
-from config.cfg import bot, dp, AvailableState as AvS
+from config.cfg import bot, dp
 from commands.setter import set_commands
-from handlers import def_router, cust_router, set_routers
+from handlers import set_routers
 
-
-# def setup_routers(dp: Dispatcher):
 
 async def main():
 	logging.basicConfig(level=logging.INFO)
 	set_routers(dp)
-	# dp.include_router(def_router)
-	# dp.include_router(cust_router)
 	await set_commands(bot)
-	# setup_routers(dp)
 	await bot.delete_webhook(drop_pending_updates=True)
 	await dp.start_polling(bot)
 
